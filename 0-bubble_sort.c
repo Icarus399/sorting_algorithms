fa5473e20915a8d1609b6c232d15b6dfef211290
Icarus399 <3530@holbertonschool.com>
2022-06-15 01:47:54 +0000

    fixing task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - function that sorts an array of intergers in ascending order
+ * using the Bubble sort algorithm
+ *@array: array
+ *@size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i = 0, j = 0, tmp;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
